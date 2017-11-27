7197d4ab0c8a9dd1a33bdc8d760e771668ca2d38
Carlos Gómez <cfgc2828@gmail.com>
2017-11-27 18:03:18 +0100

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,7 +1,10 @@
 node default {
   file { '/root/README':
-    ensure  => file,
+    ensure => file,
     content => 'This is a readme',
     owner   => 'root',
   }
+}
+node 'master.puppet.vm' {
+  include role::master_server
 }
