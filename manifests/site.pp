f2d4b742067284a7271c9a4beeaba63167154fa0
devcfgc <cfgc2828@gmail.com>
2017-11-29 17:43:58 +0100

    edit role

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => “Welcome to ${fqdn}”,
+    content => "Welcome to ${fqdn}",
     owner => 'root',
   }
 }
