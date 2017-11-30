f58319b29bb032aa2a1cc126855b890ebd99aec0
devcfgc <cfgc2828@gmail.com>
2017-11-30 14:19:28 +0100

    add parameters

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,19 @@
-class minecraft {
-  file {'/opt/minecraft':
+class minecraft (
+  $url         = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file {$install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.1.12.2.jar':
+  file {"${install_dir}/minecraft_server.1.12.2.jar":
     ensure => file,
-    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => $url,
     before => Service['minecraft'],
   }
   package {'java':
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
+  file {"${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true'
   }
@@ -23,7 +26,7 @@ class minecraft {
     enable  => true,
     require => [
       Package['java'],
-      File['/opt/minecraft/eula.txt'],
+      File["${install_dir}/eula.txt"],
       File['/etc/systemd/system/minecraft.service']
     ],
   }
