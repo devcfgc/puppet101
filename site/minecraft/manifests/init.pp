c22520e59836febe64391cd19d4e1ae5b9b1e4ba
devcfgc <cfgc2828@gmail.com>
2017-11-30 14:08:27 +0100

    add new role minecraft

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,25 +1,24 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
+  file {'/opt/minecraft/minecraft_server.1.12.2.jar':
     ensure => file,
-    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
-    ensure => file,
-    content => ‘eula=true’
+  file {'/opt/minecraft/eula.txt':
+    ensure  => file,
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
 }
-
