66f9b93db399053bf4f4ddcfc6c3991ea0644883
devcfgc <cfgc2828@gmail.com>
2017-11-30 14:04:25 +0100

    add new role minecraft

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,7 @@ node default {
     owner   => 'root',
   }
 }
+
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
@@ -13,9 +14,15 @@ node 'master.puppet.vm' {
     owner => 'root',
   }
 }
+
+node 'minetest.puppet.vm': {
+  include role::minecraft_server
+}
+
 node /^web/ {
   include role::app_server
 }
+
 node /^db/ {
   include role::db_server
 }
